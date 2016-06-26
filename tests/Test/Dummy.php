7528f26a8c32a7c6c5262c20b659df3a939713e5
Dr. Alexander Stranzky <astranzky@velia.net>
2016-06-26 18:42:55 +0200

    added basic tests for Attribute.

---
+++ tests/Test/Dummy.php
@@ -0,0 +1,94 @@
+<?php
+// Mntner.php
+
+namespace Test;
+
+use Dormilich\APNIC\Object;
+use Dormilich\APNIC\AttributeInterface as Attr;
+
+/**
+ * A stand-in for objects that don’t have the appropriate class defined.
+ */
+class Dummy extends Object
+{
+    /**
+     * Create stand-in object for any RIPE object data. 
+     * It's the responsibility of the writer to figure out the correct values.
+     * One notable difference to the regular objects is that it expects the 
+     * Type and PK names (which may be the same), and not the Type’s value.
+     * 
+     * @param string $type The object type.
+     * @param string $key The object’s primary key. If not set the Type is used.
+     */
+    public function __construct($type, $key = null)
+    {
+        $this->setType($type);
+
+        if (null === $key) {
+            $this->setKey($this->getType());
+        } else {
+            $this->setKey($key);
+        }
+
+        $this->init();
+    }
+
+    /**
+     * Create an attribute for the primary key.
+     * 
+     * @return void
+     */
+    protected function init() 
+    {
+        if ($this->getType() !== $this->getPrimaryKeyName()) {
+            // a type attribute (alternate lookup key) is usually required/single
+            $this->create($this->getType(), Attr::REQUIRED, Attr::SINGLE);
+        }
+        $this->create($this->getPrimaryKeyName(), Attr::REQUIRED, Attr::SINGLE);
+
+        // these attributes are common to all objects (as per documentation)
+        $this->create('admin-c', Attr::OPTIONAL, Attr::MULTIPLE);
+        $this->create('tech-c',  Attr::OPTIONAL, Attr::MULTIPLE);
+        $this->create('remarks', Attr::OPTIONAL, Attr::MULTIPLE);
+        $this->create('notify',  Attr::OPTIONAL, Attr::MULTIPLE);
+        $this->create('mnt-by',  Attr::REQUIRED, Attr::SINGLE);
+        $this->create('changed', Attr::REQUIRED, Attr::MULTIPLE);
+        $this->create('source',  Attr::REQUIRED, Attr::SINGLE);
+    }
+
+    /**
+     * Get an attribute. If it doesn’t exist, create it on-the-fly.
+     * 
+     * @param string $name Name of the candidate attribute.
+     * @return AttributeInterface
+     */
+    public function getAttribute($name)
+    {
+        try {
+            return parent::getAttribute($name);
+        }
+        catch (\Exception $exc) {
+            return $this->setupAttribute($name);
+        }
+    }
+
+    /**
+     * Create an Attribute. If called implicit (e.g. via setAttribute()) it 
+     * will create an optional, multiple attribute on-the-fly.
+     * 
+     * This method can be used to create an RPSL object according to a 
+     * descriptor from the metadata service.
+     * 
+     * @param string $name name of the attribute.
+     * @param boolean $required Requirement (mandatory) of the attribute.
+     * @param boolean $multiple Cardinality (multiple) of the attribute.
+     * @return AttributeInterface
+     */
+    public function setupAttribute($name, $required = Attr::OPTIONAL, $multiple = Attr::MULTIPLE)
+    {
+        $this->create($name, $required, $multiple);
+
+        // to make sure we’re not caught in a loop
+        return parent::getAttribute($name);
+    }
+}
---
+++ tests/attribute/AttributeTest.php
@@ -0,0 +1,207 @@
+<?php
+
+use Dormilich\APNIC\Attribute;
+use Dormilich\APNIC\AttributeInterface as Attr;
+
+class AttributeTest extends PHPUnit_Framework_TestCase
+{
+    public function testAttributeInterfaceIsImplemented()
+    {
+        $attr = new Attribute('foo', true, true);
+        $this->assertInstanceOf('\Dormilich\APNIC\AttributeInterface', $attr);
+    }
+
+    public function testAttributeHasCorrectName()
+    {
+        $attr = new Attribute('foo', true, true);
+        $this->assertSame('foo', $attr->getName());
+
+        $attr = new Attribute(1.8, true, true);
+        $this->assertSame('1.8', $attr->getName());
+    }
+
+    public function testAttributeIsEmptyByDefault()
+    {
+        $attr = new Attribute('foo', true, true);
+        $this->assertFalse($attr->isDefined());
+        $this->assertNull($attr->getValue());
+    }
+
+    public function constructorPropertyProvider()
+    {
+        return [
+            [true,  true, true,  true], [true,  false, true,  false], 
+            [false, true, false, true], [false, false, false, false], 
+            [0,     1,    false, true], ['x',   NULL,  true,  false],
+            [Attr::REQUIRED, Attr::SINGLE,   true,  false],
+            [Attr::OPTIONAL, Attr::MULTIPLE, false, true],
+        ];
+    }
+
+    /**
+     * @dataProvider constructorPropertyProvider
+     */
+    public function testAttributeHasCorrectPropertiesSet($required, $multiple, $expect_required, $expect_multiple)
+    {
+        $attr = new Attribute('foo', $required, $multiple);
+
+        $this->assertSame($expect_required, $attr->isRequired());
+        $this->assertSame($expect_multiple, $attr->isMultiple());
+    }
+
+    public function testAttributeConvertsInputToStrings()
+    {
+        $attr = new Attribute('foo', Attr::REQUIRED, Attr::SINGLE);
+
+        $attr->setValue(1);
+        $this->assertSame('1', $attr->getValue());
+
+        $attr->setValue(2.718);
+        $this->assertSame('2.718', $attr->getValue());
+
+        $attr->setValue('bar');
+        $this->assertSame('bar', $attr->getValue());
+
+        $test = new Test\StringObject;
+        $attr->setValue($test);
+        $this->assertSame('test', $attr->getValue());
+
+        // I am not aware that the RIPE DB uses booleans somewhere…
+        $attr->setValue(true);
+        $this->assertSame('true', $attr->getValue());
+
+        $attr->setValue(false);
+        $this->assertSame('false', $attr->getValue());
+    }
+
+    public function testNullResetsAttributeValue()
+    {
+        $attr = new Attribute('foo', Attr::REQUIRED, Attr::SINGLE);
+        $attr->setValue('foo');
+        $attr->setValue(NULL);
+        $this->assertFalse($attr->isDefined());
+    }
+
+    /**
+     * @expectedException \Dormilich\APNIC\Exceptions\InvalidDataTypeException
+     * @expectedExceptionMessageRegExp # \[foo\] #
+     */
+    public function testAttributeDoesNotAcceptResource()
+    {
+        $attr = new Attribute('foo', Attr::REQUIRED, Attr::SINGLE);
+        $attr->setValue(tmpfile());
+    }
+
+    /**
+     * @expectedException \Dormilich\APNIC\Exceptions\InvalidDataTypeException
+     * @expectedExceptionMessageRegExp # \[foo\] #
+     */
+    public function testAttributeDoesNotAcceptObject()
+    {
+        $attr = new Attribute('foo', Attr::REQUIRED, Attr::SINGLE);
+        $attr->setValue(new stdClass);
+    }
+
+    public function testSingleAttributeOnlyHasOneValue()
+    {
+        $attr = new Attribute('foo', Attr::REQUIRED, Attr::SINGLE);
+
+        $attr->setValue('fizz');
+        $this->assertSame('fizz', $attr->getValue());
+
+        $attr->setValue('buzz');
+        $this->assertSame('buzz', $attr->getValue());
+
+        $attr->addValue('bar');
+        $this->assertSame('bar', $attr->getValue());
+    }
+
+    /**
+     * @expectedException \Dormilich\APNIC\Exceptions\InvalidDataTypeException
+     */
+    public function testSingleAttributeDoesNotAllowArrayInput()
+    {
+        $attr = new Attribute('foo', Attr::REQUIRED, Attr::SINGLE);
+        $attr->setValue(['fizz', 'buzz']);
+    }
+
+    public function testMultipleAttributeReturnsList()
+    {
+        $attr = new Attribute('foo', Attr::REQUIRED, Attr::MULTIPLE);
+
+        $attr->addValue('fizz');
+        $this->assertSame(['fizz'], $attr->getValue());
+
+        $attr->addValue('buzz');
+        $this->assertSame(['fizz', 'buzz'], $attr->getValue());
+    }
+
+    public function testSetValueResetsAttributeValue()
+    {
+        $attr = new Attribute('foo', Attr::REQUIRED, Attr::MULTIPLE);
+
+        $attr->setValue('fizz');
+        $this->assertSame(['fizz'], $attr->getValue());
+
+        $attr->setValue('buzz');
+        $this->assertSame(['buzz'], $attr->getValue());
+    }
+
+    public function testMultipleAttributeAllowsStringArray()
+    {
+        $attr = new Attribute('foo', Attr::REQUIRED, Attr::MULTIPLE);
+
+        $attr->setValue(['fizz', 'buzz']);
+        $this->assertSame(['fizz', 'buzz'], $attr->getValue());
+    }
+
+    /**
+     * @expectedException \Dormilich\APNIC\Exceptions\InvalidDataTypeException
+     */
+    public function testMultipleAttributeDoesNotAllowNonScalarArray()
+    {
+        $attr = new Attribute('foo', Attr::REQUIRED, Attr::MULTIPLE);
+        $attr->setValue([NULL]);
+    }
+
+    /**
+     * @expectedException \Dormilich\APNIC\Exceptions\InvalidDataTypeException
+     */
+    public function testMultipleAttributeDoesNotAllowNestedArray()
+    {
+        $attr = new Attribute('foo', Attr::REQUIRED, Attr::MULTIPLE);
+        $attr->setValue(['bar', [1,2,3]]);
+    }
+
+    public function testMultipleAttributeIgnoresArrayKeys()
+    {
+        $attr = new Attribute('foo', Attr::REQUIRED, Attr::MULTIPLE);
+
+        $attr->setValue(['fizz' => 'buzz']);
+        $this->assertSame(['buzz'], $attr->getValue());
+    }
+
+    public function testAttributeCanBeLocked()
+    {
+        $attr = new Attribute('test', Attr::REQUIRED, Attr::SINGLE);
+        $attr->lock();
+
+        $attr->setValue('foo');
+        $this->assertSame('foo', $attr->getValue());
+
+        $attr->setValue('bar');
+        $this->assertSame('foo', $attr->getValue());
+    }
+
+    public function testMultipleAttributeCanBeLocked()
+    {
+        $attr = new Attribute('test', Attr::REQUIRED, Attr::MULTIPLE);
+        $attr->lock();
+
+        $attr->setValue(['foo', 'bar']);
+        $this->assertSame(['foo', 'bar'], $attr->getValue());
+
+        $attr->addValue('quux');
+        $this->assertSame(['foo', 'bar'], $attr->getValue());
+    }
+}
